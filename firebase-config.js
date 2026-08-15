3eb01d31e12ce66ed86d984f40f7b06b66bd0e5e
ModernChess <taiyeebmuhtadi@gmail.com>
2026-08-16 01:26:40 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
